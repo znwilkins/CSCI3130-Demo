49e452b87cd3002eef655312ba7f62a122da8a06
Zachary Wilkins <znwilkins@gmail.com>
2018-01-18 20:44:59 -0400

    Connect button click to displayText

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * This method handles the "Go!" button, and displays
+     * text the user has typed into the text field.
+     */
     public void displayText(View view) {
         EditText editText = findViewById(R.id.editText);
         String msg = editText.getText().toString();
