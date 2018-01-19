7475f27f46b7c6e1ca6444f77950d38cfe6c90f0
Zachary Wilkins <znwilkins@gmail.com>
2018-01-18 20:16:48 -0400

    Add button and textfield to main activity

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void displayText(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String msg = editText.getText().toString();
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(msg);
+    }
+
 }
